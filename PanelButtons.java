b84a7fb87b187b1fa0c0715e14bad92420d468f8
AntonDanylenko <adanylenko@stuy.edu>
2018-01-08 14:46:55 -0500

    Added a second panel but button doesnt work now

--- PanelButtons.java
+++ PanelButtons.java
@@ -4,10 +4,10 @@ import java.awt.event.*;
 
 public class PanelButtons extends JFrame implements ActionListener{
     private JFrame frame;
-    private JPanel panel0;
-    private JButton t1;
+    private JPanel panel0, panel1;
+    private JButton t1, t2;
     private JTextField tf1;
-    private JLabel l1;
+    private JLabel l1, l2;
     
     public PanelButtons() {
     frame = new JFrame("Window");
@@ -15,21 +15,25 @@ public class PanelButtons extends JFrame implements ActionListener{
 
 
     panel0 = new JPanel();
+    panel1 = new JPanel();
 
     panel0.setLayout(new FlowLayout());
 
     t1 = new JButton("Button");
+    t2 = new JButton("Other Button");
     tf1 = new JTextField(16);
     l1 = new JLabel("This is a label");
-
+    l2 = new JLabel("This is also a label");
 
     t1.addActionListener(this);
     tf1.addActionListener(this);
-
+    t2.addActionListener(this);
 
     panel0.add(t1);
     panel0.add(tf1);
     panel0.add(l1);
+    panel1.add(t2);
+    panel1.add(l2);
 
     frame.setContentPane(panel0);
     frame.setSize(500,500);
@@ -40,8 +44,13 @@ public class PanelButtons extends JFrame implements ActionListener{
     public void actionPerformed(ActionEvent e) {
 	JButton button = (JButton) e.getSource();
 	if (button == t1){
-	    tf1.setText("You pressed the button");
+	    frame.remove(panel0);
+	    frame.setContentPane(panel1);
 	}
+	/*else if (button == t2){
+	    frame.remove(panel1);
+	    frame.setContentPane(panel0);
+	    }*/
     }
 
     public static void main(String[]args){
